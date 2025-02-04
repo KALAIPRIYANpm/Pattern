3e18169c31afb52342ce2f79fc4636d4d8730c97
KALAIPRIYAMpm <kalaipriyan7777@gmail.com>
2025-02-04 10:37:19 +0530

    second commit

---
+++ src/basic/in/Kalai.java
@@ -0,0 +1,16 @@
+//package basic.in;
+//
+//public class Kalai {
+//
+//    pattern(5);
+//}
+//
+//
+//public static void pattern(int n){
+//    for(int i =1;i<=n;i++){
+//        for(int j = i;j<=n;j++){
+//            System.out.print(" * ");
+//        }
+//        System.out.println("");
+//    }
+//}
