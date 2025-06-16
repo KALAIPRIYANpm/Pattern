5d5e9c797831bb0e5b0783b55fa186cd4226282a
KALAIPRIYAMpm <kalaipriyan7777@gmail.com>
2025-06-16 11:12:31 +0530

    feat:code

---
+++ src/train.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class train {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+//        int roll_no = input.nextInt();
+//        System.out.println(roll_no);
+        String name = input.nextLine();
+        System.out.println(name);
+
+        for(int i =0 ; i<5 ; i++){
+            System.out.println("Hello World");
+
+        }
+
+    }
+}
